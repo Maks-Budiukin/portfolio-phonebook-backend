de31a12c27e38677bf3aad4be789811e124aab37
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2023-08-31 14:16:24 +0300

    added new fields for contacts

--- src/contacts/contacts.model.ts
+++ src/contacts/contacts.model.ts
@@ -25,6 +25,60 @@ export class Contact {
   number: string;
 
   @ApiResponseProperty({
+    example: '@jhets',
+  })
+  @Prop({ default: null })
+  telegram?: string;
+
+  @ApiResponseProperty({
+    example: 'https://www.linkedin.com/in/james-hetfield',
+  })
+  @Prop({ default: null })
+  linkedin?: string;
+
+  @ApiResponseProperty({
+    example: '@j.hets',
+  })
+  @Prop({ default: null })
+  instagram?: string;
+
+  @ApiResponseProperty({
+    example: 'https://github.com/James-Hetfield/',
+  })
+  @Prop({ default: null })
+  github?: string;
+
+  @ApiResponseProperty({
+    example: 'https://www.facebook.com/mr.hetfield/',
+  })
+  @Prop({ default: null })
+  facebook?: string;
+
+  @ApiResponseProperty({
+    example: 'https://twitter.com/therealhetfield',
+  })
+  @Prop({ default: null })
+  twitter?: string;
+
+  @ApiResponseProperty({
+    example: 'https://bitbucket.org/JHetf/',
+  })
+  @Prop({ default: null })
+  bitbucket?: string;
+
+  @ApiResponseProperty({
+    example: '+1-505-555-14875',
+  })
+  @Prop({ default: null })
+  viber?: string;
+
+  @ApiResponseProperty({
+    example: '+1-505-555-14875',
+  })
+  @Prop({ default: null })
+  whatsapp?: string;
+
+  @ApiResponseProperty({
     example: '64e7b40704f6b0d4d0440b26',
   })
   @Prop({ type: Types.ObjectId, ref: User.name })
--- src/contacts/dto/update-contacts.dto.ts
+++ src/contacts/dto/update-contacts.dto.ts
@@ -7,7 +7,6 @@ export class UpdateContactDto {
     description: "Contact's name",
   })
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   name: string;
 
@@ -16,7 +15,78 @@ export class UpdateContactDto {
     description: 'Phone number',
   })
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   number: string;
+
+  @ApiProperty({
+    example: '@jhets',
+    description: "Contact's Telegram",
+  })
+  @IsOptional()
+  @IsString()
+  telegram?: string;
+
+  @ApiProperty({
+    example: 'https://www.linkedin.com/in/james-hetfield',
+    description: "Contact's LinkedIn",
+  })
+  @IsOptional()
+  @IsString()
+  linkedin?: string;
+
+  @ApiProperty({
+    example: '@j.hets',
+    description: "Contact's Instagram",
+  })
+  @IsOptional()
+  @IsString()
+  instagram?: string;
+
+  @ApiProperty({
+    example: 'https://github.com/James-Hetfield/',
+    description: "Contact's GitHub",
+  })
+  @IsOptional()
+  @IsString()
+  github?: string;
+
+  @ApiProperty({
+    example: 'https://www.facebook.com/mr.hetfield/',
+    description: "Contact's Facebook",
+  })
+  @IsOptional()
+  @IsString()
+  facebook?: string;
+
+  @ApiProperty({
+    example: 'https://twitter.com/therealhetfield',
+    description: "Contact's Twitter",
+  })
+  @IsOptional()
+  @IsString()
+  twitter?: string;
+
+  @ApiProperty({
+    example: 'https://bitbucket.org/JHetf/',
+    description: "Contact's Bitbucket",
+  })
+  @IsOptional()
+  @IsString()
+  bitbucket?: string;
+
+  @ApiProperty({
+    example: '+1-505-555-14875',
+    description: "Contact's Viber",
+  })
+  @IsOptional()
+  @IsString()
+  viber?: string;
+
+  @ApiProperty({
+    example: '+1-505-555-14875',
+    description: "Contact's WhatsApp",
+  })
+  @IsOptional()
+  @IsString()
+  whatsapp?: string;
 }
